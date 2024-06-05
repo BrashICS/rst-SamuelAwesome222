f9f90d311408ae9e9b6ff655ff020cc11ba62c3d
Sam Black <samuel.blackadar@stu.ocsb.ca>
2024-06-05 08:38:27 -0400

    notdone

--- innerworkings.js
+++ innerworkings.js
@@ -1,1 +1,10 @@
 const coloumns
+document.getElementById("column1").addEventListener("click", increment)
+document.getElementById("column2").addEventListener("click", increment)
+document.getElementById("column3").addEventListener("click", increment)
+document.getElementById("column4").addEventListener("click", increment)
+document.getElementById("column5").addEventListener("click", increment)
+document.getElementById("column6").addEventListener("click", increment)
+document.getElementById("column7").addEventListener("click", increment)
+
+function 
