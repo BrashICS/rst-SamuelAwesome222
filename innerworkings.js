acb4c689056db2cd82ceaa4f514635c5f6fd9542
SamuelAwesome222 <159923397+SamuelAwesome222@users.noreply.github.com>
2024-06-06 13:51:08 +0000

    notdone

--- innerworkings.js
+++ innerworkings.js
@@ -7,4 +7,10 @@ document.getElementById("column5").addEventListener("click", increment)
 document.getElementById("column6").addEventListener("click", increment)
 document.getElementById("column7").addEventListener("click", increment)
 
-function 
+col1 = 0
+col2 = 0
+col3 = 0
+col4 = 0
+col5 = 0
+col6 = 0
+col7 = 0
