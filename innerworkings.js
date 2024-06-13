11f71992e65c495c9f0e51ee9512de1ea542ef8e
SamuelAwesome222 <159923397+SamuelAwesome222@users.noreply.github.com>
2024-06-13 13:48:39 +0000

    notdone

--- innerworkings.js
+++ innerworkings.js
@@ -1,16 +1,26 @@
-const coloumns
-document.getElementById("column1").addEventListener("click", increment)
-document.getElementById("column2").addEventListener("click", increment)
-document.getElementById("column3").addEventListener("click", increment)
-document.getElementById("column4").addEventListener("click", increment)
-document.getElementById("column5").addEventListener("click", increment)
-document.getElementById("column6").addEventListener("click", increment)
-document.getElementById("column7").addEventListener("click", increment)
+const columns = 0
 
-col1 = 0
-col2 = 0
-col3 = 0
-col4 = 0
-col5 = 0
-col6 = 0
-col7 = 0
+
+document.getElementById("column1").addEventListener("click", col1add)
+
+
+console.log("Hello World!");
+
+let col1 = 0
+let col2 = 0
+let col3 = 0
+let col4 = 0
+let col5 = 0
+let col6 = 0
+let col7 = 0
+
+function col1add(){
+  col1 = col1+1
+  document.getElementById("column1Count").innerText = col1;
+
+}
+
+if(col1 = 1){
+  document.getElementById("17").innerHTML = "R"
+}
+
